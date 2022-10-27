f7673fd4e6954aa4970b8a01663ad262e374bdf6
lemosjuliana <lemos.julianajesus@gmail.com>
2022-10-27 20:56:44 -0300

    commit

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,24 +1,23 @@
 
-const form = document.getElementById('form');
-form.addEventListener('submit', function(event)
-{ 
-    event.preventDefault();
-    const temperature = document.getElementById('temperature').value;
-    const wind = document.getElementById('wind').value;
+const temperatureCelsius = 33;
+const windSpeedKmH = 2.5;
 
-    if (temperature <= 50 && wind > 3)
-    {
-    const windChill = (35.74 + (0.6215 * temperature))-(35.75 * Math.pow(wind,0.16)) + 
-    (0.4275 * temperature * Math.pow(wind,0.16));
-    const windChillRounded = Math.round(windChill);
-    document.getElementById("windChill").innerHTML = windChillRounded;
-    }
-    else
-    {
-    notApplicable = "N/A";
-    document.getElementById("windChill").innerHTML = notApplicable;
-    }
+const temperatureFahrenheit = temperatureCelsius * (9/5) + 32;
+const windSpeedMH = windSpeedKmH / 1.609;
 
-});
+document.querySelector('.temperature').textContent = temperatureCelsius;
+document.querySelector('.wind').textContent = windSpeedKmH;
+
+if (temperatureFahrenheit <= 50 && windSpeedMH > 3)
+{
+const windChill = (35.74 + (0.6215 * temperatureFahrenheit))-(35.75 * Math.pow(windSpeedMH,0.16)) + 
+(0.4275 * temperatureFahrenheit * Math.pow(windSpeedMH,0.16));
+const windChillRounded = Math.round(windChill);
+document.querySelectorAll('.windChill').textContent = windChillRounded;
+}
+else
+{
+document.querySelector('.windChill').textContent = 'N/A';
+}
 
 
