15310fe067e091c14a01fb086bcf65e5d58e938c
lemosjuliana <lemos.julianajesus@gmail.com>
2022-11-19 01:13:23 -0300

    commit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -9,12 +9,12 @@ x.onclick = toggleMenu;
 
 // Last Modified
 
-const datefield = document.getElementById("date");
+const datefield = document.querySelector(".date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 now
 );
-datefield.innerHTML = `<em>${fulldate}</em>`;
+datefield.innerHTML = fulldate;
 
 
 const dateTimeLastModified = `Last modification: ${document.lastModified}`;
