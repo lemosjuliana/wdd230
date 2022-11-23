5fced0a6354773895edbd988925b9f6ba51bc9ef
lemosjuliana <lemos.julianajesus@gmail.com>
2022-11-22 22:53:09 -0300

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -42,6 +42,10 @@ card.appendChild(phoneNumber);
 card.appendChild(website);
 
 // Add/append the existing HTML div with the cards class with the section(card)
+document.querySelector('.spotlight').appendChild(card);
+  
+
+// Add/append the existing HTML div with the cards class with the section(card)
 const divGrid = document.querySelector('div.grid');
 if (divGrid) divGrid.appendChild(card);
 }
@@ -69,6 +73,3 @@ if (display) {
 }
 }
 
-  // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('.spotlight').appendChild(cards);
-  
